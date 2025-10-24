a4fa3f9961279aaab612c019f9d2beb266e5e847
lukasuf <lukaszhukauskas@ufl.edu>
2025-10-24 17:55:57 -0400

    Add file input and data filtering to mainBPT.cpp for more advanced testing.cpp
    Uses a regex filter to filter data set to only include words with lower-case English letters with atleast 3 letters.
    
    Uses a for loop for filenames to access each "_ Words.txt" file and add all valid words to the words vector.

--- mainBPT.cpp
+++ mainBPT.cpp
@@ -9,12 +9,56 @@
 
 int main()
 {
-    std::vector<std::string> words = {"app", "appl", "ban", "baa", "bana", "basi", "best", "brea", "bat", "boss", "band", "cat", "catr", "dog", "dove"};
+    std::vector<std::string> words;
+
+    // Define the regex pattern for valid autocomplete options
+    // [a-z] - alphabetical; {3,} - atleast 3 letters; ^/$ - together upgrades atleast to all letters
+    std::regex filter("(^[a-z]{3,}$)");
+
+    // For each letter in the alphabet,
+    for (char l = 'A'; l <= 'Z'; l++) {
+        // construct a filename for that letter _, yielding "_ Words.txt"
+        std::string filename = "data/" + std::string(1, l) + " Words.txt";
+
+        // Open the file at that filename
+        std::ifstream file(filename);
+
+        // If it did not open,
+        if (!file.is_open())
+        {
+            // print error and continue to next letter
+            std::cerr << "Unable to open file " << filename << std::endl;
+            continue;
+        }
+
+        // If it did, put each line through the filter, adding valid words to words vector
+        std::string line;
+        while (std::getline(file, line))
+        {
+            std::istringstream stream(line);
+            std::string word;
+            while (stream >> word)
+            {
+                if (std::regex_match(word, filter))
+                {
+                    words.push_back(word);
+                }
+            }
+        }
+
+        // Close file before exiting iteration
+        file.close();
+    }
+
+
+    //DEBUG: Easier Structure Checking
+    words = {"app", "appl", "ban", "baa", "bana", "basi", "best", "brea", "bat", "boss", "band", "cat", "catr", "dog", "dove"};
     //ACCORDING TO VISUALIZER, SHOULD MATCH   <basi>
     //                                        <baa bana> <best brea catr>
     //                                        <app appl> <baa ban> <bana band> <basi bat> <best boss> <brea cat> <catr dog dove>
 
-    //Check words size
+
+    //DEBUG: Check words size
     std::cout << "Words Size: " << words.size() << std::endl << std::endl;
 
     //Initialize n-Order B+ Tree
@@ -27,7 +71,7 @@ int main()
         myBPT.insert(word);
     }
 
-    //Check B+ tree structure
+    //DEBUG: Check B+ tree structure
     std::cout << "B+ Tree structure:" << std::endl;
     myBPT.printBPT();
 
