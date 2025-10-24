c6813acc2c98d62ef8035eff34a7a6967834c602
lukasuf <lukaszhukauskas@ufl.edu>
2025-10-24 17:37:30 -0400

    Add Basic Insertion Function to BPT.cpp (BPT Implementation)

--- BPT.cpp
+++ BPT.cpp
@@ -43,3 +43,43 @@ BPT::~BPT()
 
     root = nullptr;
 }
+
+// main() insertion: hides Node structure and handles non-splitting insertion
+void BPT::insert(const std::string& key)
+{
+    Node* currentNode = root;
+
+    // Search for the correct insertion point
+    // While the current node is neither null nor a leaf,
+    while ((currentNode != nullptr) && (currentNode->leafNode == false))
+    {
+        int i = 0;
+        // iterate through the keys of the current node until the prefix is less than the ith key.
+        while (i < currentNode->keys.size() && key >= currentNode->keys[i])
+        {
+            i++;
+        }
+        // This should yield the index of the child that has keys that are either greater than or equal to the key.
+        // So, update the current node to the child at that index.
+        currentNode = currentNode->children[i];
+    }
+
+    // Start at the first element in the keys vector of the currentNode
+    auto iter = currentNode->keys.begin();
+    // While there are still more keys and the current key is less than the insertion key,
+    while ((iter != currentNode->keys.end()) && (*iter < key))
+    {
+        // increment the iterator and index.
+        iter++;
+    }
+
+    // Then, insert key at proper node.
+    currentNode->keys.insert(iter, key);
+
+    // After insertion, if the current number of keys violates maximum key rule,
+    if (currentNode->keys.size() >= order)
+    {
+        // we must split it.
+        splitNode(currentNode);
+    }
+}
